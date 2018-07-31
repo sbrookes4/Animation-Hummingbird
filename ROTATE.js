7be73f55d693841ac41f8fc31745ccb6b4e90c77
sbrookes4 <shawnabrookes@gmail.com>
2018-07-30 19:08:41 -0500

    Created this demo in about 30 minutes with knowing just 1 week of jQuery

---
+++ ROTATE.js
@@ -0,0 +1,19 @@
+//alert("hello");
+//Created this demo in about 30 minutes with knowing just 1 week of jQuery
+
+$(document).ready(function() {
+
+$("button").click(function(){
+
+	for(i = 0; i < 1000; i++){
+
+	$("#hum").animate({left: "70vw"},2000);
+	$("#hum").animate({top: "29vw"},2000);
+	$("#hum").animate({left: "30px"},2000);
+	$("#hum").animate({top: "60px"},2000);
+
+		}
+
+	});
+
+ });
